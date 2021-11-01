b7487244b7e7875cefccdc27da827dcb26ba96c6
dbonach <deivitlopes@gmail.com>
2021-11-01 16:35:55 -0300

    Project created

---
+++ src/main/java/com/estudandojava/spring/NelioJavaSpringMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.estudandojava.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NelioJavaSpringMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NelioJavaSpringMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudandojava/spring/NelioJavaSpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudandojava.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NelioJavaSpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
