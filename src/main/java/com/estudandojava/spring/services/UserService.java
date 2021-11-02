fd41ddfd9c0d9b07c4a1e23665577aa1d0b8386c
dbonach <deivitlopes@gmail.com>
2021-11-01 22:27:24 -0300

    Obtendo um usuario por id

--- src/main/java/com/estudandojava/spring/services/UserService.java
+++ src/main/java/com/estudandojava/spring/services/UserService.java
@@ -1,12 +1,14 @@
 package com.estudandojava.spring.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.estudandojava.spring.domain.User;
 import com.estudandojava.spring.repository.UserRepository;
+import com.estudandojava.spring.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return userRepository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = userRepository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(id));
 	}
 }
